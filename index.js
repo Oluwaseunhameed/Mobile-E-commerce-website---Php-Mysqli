034f312802943526cad0c354537c8ed8f0c7abbe
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-06-17 13:53:08 +0100

    Shopping cart page added

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ $(document).ready(function () {
 
   // click on qty up button
   $qty_up.click(function (e) {
+    let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
     if ($input.val() >= 1 && $input.val() <= 9) {
       $input.val(function (i, oldval) {
         return ++oldval;
@@ -84,6 +85,7 @@ $(document).ready(function () {
 
   // click on qty down button
   $qty_down.click(function (e) {
+    let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
     if ($input.val() > 1 && $input.val() <= 10) {
       $input.val(function (i, oldval) {
         return --oldval;
