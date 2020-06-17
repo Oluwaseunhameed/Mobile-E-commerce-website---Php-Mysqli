57e08c2ce8bc21118388061e5f06aced9d4538fb
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-06-17 17:38:23 +0100

    fetching product from database

--- Template/_new-phones.php
+++ Template/_new-phones.php
@@ -1,111 +1,19 @@
 <!-- New Phones -->
+<?php
+shuffle($product_shuffle);
+?>
 <section id="new-phones">
     <div class="container">
         <h4 class="font-rubik font-size-20">New Phones</h4>
         <hr>
         <!-- owl carousel -->
         <div class="owl-carousel owl-theme">
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/1.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Samsung Galaxy 10</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>N57,000</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/2.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Readme Note 7</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>N57,000</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/3.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Readme Note 7</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>N57,000</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/6.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Samsung Galaxy 10</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>N57,000</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/1.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Readme Note 7</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>N57,000</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <div class="d-flex flex-column">
-                        <a href="#"><img src="./assets/products/1.png" class="img-fluid" alt="pro1"></a>
+            <?php foreach ($product_shuffle as $item) { ?>
+                <div class="item py-2 bg-light">
+                    <div class="product font-rale">
+                        <a href="#"><img src="<?php echo $item['item_image'] ?? "./assets/products/1.png"; ?>" alt="product1" class="img-fluid"></a>
                         <div class="text-center">
-                            <h6>Readme Note 7</h6>
+                            <h6><?php echo  $item['item_name'] ?? "Unknown";  ?></h6>
                             <div class="rating text-warning font-size-12">
                                 <span><i class="fas fa-star"></i></span>
                                 <span><i class="fas fa-star"></i></span>
@@ -114,34 +22,14 @@
                                 <span><i class="far fa-star"></i></span>
                             </div>
                             <div class="price py-2">
-                                <span>N47,000</span>
+                                <span>$<?php echo $item['item_price'] ?? '0'; ?></span>
                             </div>
                             <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <div class="d-flex flex-column">
-                        <a href="#"><img src="./assets/products/2.png" class="img-fluid" alt="pro1"></a>
-                        <div class="text-center">
-                            <h6>Readme Note 7</h6>
-                            <div class="rating text-warning font-size-12">
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="far fa-star"></i></span>
-                            </div>
-                            <div class="price py-2">
-                                <span>N47,000</span>
-                            </div>
-                            <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php } // closing foreach function 
+            ?>
         </div>
         <!-- !owl carousel -->
     </div>
